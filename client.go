4f24ca81c2e53ee39013b4f69b071ceed423830d
Brandon High <bhigh@freaks.com>
2020-04-06 15:43:13 -0700

    Add sleep and retries for 429 responses. Retry with backoff on 5xx errors. Add logging for debugging.

--- client.go
+++ client.go
@@ -3,10 +3,14 @@ package dropbox
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"strconv"
 	"strings"
+	"time"
 )
 
 // Client implements a Dropbox client. You may use the Files and Users
@@ -72,8 +76,39 @@ func (c *Client) download(path string, in interface{}, r io.Reader) (io.ReadClos
 
 // perform the request.
 func (c *Client) do(req *http.Request) (io.ReadCloser, int64, error) {
-	res, err := c.HTTPClient.Do(req)
+	var err error
+	var res *http.Response
+	error_retry_time := 0.5
+request_loop:
+	for error_retry_time < 300 {
+		res, err = c.HTTPClient.Do(req)
+		switch res.StatusCode {
+		case 429:
+			log.Print(fmt.Sprintf("Received Retry status code %d.", res.StatusCode))
+			sleep_time, conv_e := strconv.Atoi(res.Header.Get("Retry-After"))
+			if conv_e != nil {
+				sleep_time = 60
+			}
+			log.Print(fmt.Sprintf("Sleeping for %d seconds.", sleep_time))
+			time.Sleep(time.Duration(sleep_time) * time.Second)
+		case 500:
+			log.Print(fmt.Sprintf("Received Error status code %d.", res.StatusCode))
+			log.Print(fmt.Sprintf("Sleeping for %d seconds.", error_retry_time))
+			time.Sleep(time.Duration(error_retry_time) * time.Second)
+			error_retry_time *= 1.5
+		default:
+			break request_loop
+		}
+	}
 	if err != nil {
+		log.Print(fmt.Sprintf("URL: %s - Method: %s", req.URL, req.Method))
+		log.Print(fmt.Sprintf("HTTP Error StatusCode %d", res.StatusCode))
+		if b, err := ioutil.ReadAll(res.Body); err == nil {
+			log.Print(string(b))
+		} else {
+			log.Print(fmt.Sprintf("Error reading body: %s", err))
+
+		}
 		return nil, 0, err
 	}
 
@@ -93,6 +128,9 @@ func (c *Client) do(req *http.Request) (io.ReadCloser, int64, error) {
 	if strings.Contains(kind, "text/plain") {
 		if b, err := ioutil.ReadAll(res.Body); err == nil {
 			e.Summary = string(b)
+			log.Print(fmt.Sprintf("URL: %s - Method: %s", req.URL, req.Method))
+			log.Print(fmt.Sprintf("HTTP StatusCode %d", res.StatusCode))
+			log.Print(e.Summary)
 			return nil, 0, e
 		} else {
 			return nil, 0, err
@@ -100,8 +138,13 @@ func (c *Client) do(req *http.Request) (io.ReadCloser, int64, error) {
 	}
 
 	if err := json.NewDecoder(res.Body).Decode(e); err != nil {
+		log.Print(fmt.Sprintf("URL: %s - Method: %s", req.URL, req.Method))
+		log.Print(fmt.Sprintf("HTTP StatusCode %d", res.StatusCode))
+		log.Print(e.Summary)
 		return nil, 0, err
 	}
-
+	log.Print(fmt.Sprintf("URL: %s - Method: %s", req.URL, req.Method))
+	log.Print(fmt.Sprintf("HTTP StatusCode %d", res.StatusCode))
+	log.Print(e.Summary)
 	return nil, 0, e
 }
